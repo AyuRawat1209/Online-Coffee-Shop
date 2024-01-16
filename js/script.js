bfa3acb7f5dcd262af7424c3bb954f0fc49cbf98
Ayush Rawat <146112122+AyuRawat1209@users.noreply.github.com>
2024-01-16 20:31:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+
+    cartitem.classList.remove('active');
+    searchform.classList.remove('active');
+}
+
+let cartitem = document.querySelector('.cart-item-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartitem.classList.toggle('active');
+
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+}
+
+let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchform.classList.toggle('active');
+
+    navbar.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    cartitem.classList.remove('active');
+    searchform.classList.remove('active');
+}
